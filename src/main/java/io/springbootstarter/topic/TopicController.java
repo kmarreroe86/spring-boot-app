5643e266f262e3c981950e3e22305dd5b03fdb89
karel <kmarreroe86@gmail.com>
2018-02-01 20:30:52 -0500

    Code formatting.

--- src/main/java/io/springbootstarter/topic/TopicController.java
+++ src/main/java/io/springbootstarter/topic/TopicController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
-	
+
 	@Autowired
 	private TopicService topicService;
 
@@ -19,25 +19,25 @@ public class TopicController {
 	public List<Topic> getAllTopics() {
 		return topicService.getAllTopics();
 	}
-	
+
 	@RequestMapping("/topic/{id}")
 	public Topic getTopicById(@PathVariable String id) {
 		return topicService.getTopicById(id);
 	}
-	
-	@RequestMapping(method=RequestMethod.POST, value="/topics")
-	public void addTopic(@RequestBody Topic topic){
-		
+
+	@RequestMapping(method = RequestMethod.POST, value = "/topics")
+	public void addTopic(@RequestBody Topic topic) {
+
 		topicService.addTopic(topic);
 	}
-	
-	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
-	public void updateTopic(@RequestBody Topic topic, @PathVariable String id){
-		
+
+	@RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+
 		topicService.updateTopic(id, topic);
 	}
-	
-	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
 	public void deleteTopicById(@PathVariable String id) {
 		topicService.deleteTopicByIdd(id);
 	}
